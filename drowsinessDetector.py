507d9518bd2681215bcff0ffd8719c06f3ac4d2c
Icosang <you8147@naver.com>
2021-05-16 18:53:48 +0900

    기울기 감지 코드 추가

--- drowsinessDetector.py
+++ drowsinessDetector.py
@@ -3,11 +3,11 @@ import dlib
 from scipy.spatial import distance
 
 def calculate_EAR(eye):
-	A = distance.euclidean(eye[1], eye[5])
-	B = distance.euclidean(eye[2], eye[4])
-	C = distance.euclidean(eye[0], eye[3])
-	ear_aspect_ratio = (A+B)/(2.0*C)
-	return ear_aspect_ratio
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear_aspect_ratio = (A+B)/(2.0*C)
+    return ear_aspect_ratio
 
 cap = cv2.VideoCapture(0)
 hog_face_detector = dlib.get_frontal_face_detector()
@@ -25,26 +25,26 @@ while True:
         rightEye = []
 
         for n in range(36,42):
-        	x = face_landmarks.part(n).x
-        	y = face_landmarks.part(n).y
-        	leftEye.append((x,y))
-        	next_point = n+1
-        	if n == 41:
-        		next_point = 36
-        	x2 = face_landmarks.part(next_point).x
-        	y2 = face_landmarks.part(next_point).y
-        	cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            leftEye.append((x,y))
+            next_point = n+1
+            if n == 41:
+                next_point = 36
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
 
         for n in range(42,48):
-        	x = face_landmarks.part(n).x
-        	y = face_landmarks.part(n).y
-        	rightEye.append((x,y))
-        	next_point = n+1
-        	if n == 47:
-        		next_point = 42
-        	x2 = face_landmarks.part(next_point).x
-        	y2 = face_landmarks.part(next_point).y
-        	cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            rightEye.append((x,y))
+            next_point = n+1
+            if n == 47:
+                next_point = 42
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
 
         left_ear = calculate_EAR(leftEye)
         right_ear = calculate_EAR(rightEye)
@@ -52,12 +52,30 @@ while True:
         EAR = (left_ear+right_ear)/2
         EAR = round(EAR,2)
         if EAR<0.26:
-        	cv2.putText(frame,"DROWSY",(20,100),
-        		cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),4)
-        	cv2.putText(frame,"Are you Sleepy?",(20,400),
-        		cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),4)
-        	print("Drowsy")
-        print(EAR)
+            cv2.putText(frame,"DROWSY",(20,100),
+                cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),4)
+            cv2.putText(frame,"Are you Sleepy?",(20,400),
+                cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),4)
+            print("Drowsy")
+
+		# eye inclination check
+        x1 = face_landmarks.part(39).x - face_landmarks.part(36).x
+        y1 = face_landmarks.part(39).y - face_landmarks.part(36).y
+        incl1 = y1 / x1
+
+        x2 = face_landmarks.part(45).x - face_landmarks.part(42).x
+        y2 = face_landmarks.part(45).y - face_landmarks.part(42).y
+        incl2 = y2 / x2
+
+        incl = abs((incl1+incl2)/2)
+
+        if incl > 0.5:
+            cv2.putText(frame, "Incl Detection", (20, 100),
+                cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 4)
+            cv2.putText(frame, "Are you Sleepy?", (20, 400),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 4)
+            print("Drowsy")
+        #print(EAR)
 
     cv2.imshow("Are you Sleepy", frame)
 
